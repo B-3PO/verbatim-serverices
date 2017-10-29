45d5086813362df6d58cf54d1ecc92fb5407fd68
Ben Rubin <ben@benjaminrubin.com>
2017-10-29 18:43:18 -0500

    add url decoding

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const port = process.env.PORT || 5000;
 let app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // -- start server
 app.listen(port, () => {
