f2bf7a1d2229b49590c8d6b5b9b24b5620125295
Ben Rubin <benjaminrubin@Benjamins-MacBook-Pro.local>
2017-10-28 21:21:06 -0500

    get-cart

--- server.js
+++ server.js
@@ -66,3 +66,44 @@ app.post('/add-item', (req, res) => {
   ]
   });
 });
+
+
+app.post('/get-cart', {
+   "messages": [
+      {
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"list",
+            "top_element_style":"large",
+            "elements":[
+              {
+                "title":"Chatfuel Rockets T-Shirt",
+                "image_url":"http://rockets.chatfuel.com/img/shirt.png",
+                "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://rockets.chatfuel.com/store/shirt",
+                    "title":"View Item"
+                  }
+                ]
+              },
+              {
+                "title":"Chatfuel Rockets Hoodie",
+                "image_url":"http://rockets.chatfuel.com/img/hoodie.png",
+                "subtitle":"Soft gray cotton t-shirt with CF Rockets logo",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://rockets.chatfuel.com/store/hoodie",
+                    "title":"View Item"
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      }
+    ]
+  });
