499d21174591833009cf281fa11252ce138e07b7
Ben Rubin <benjaminrubin@Benjamins-MacBook-Pro.local>
2017-10-28 20:54:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+
+const port = 8080;
+let app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(compression());
+app.options('*', cors());
+
+app.use('/status', (req, res) => {
+  res.send('ok');
+});
+
+app.listen(port, () => {
+  console.log(`running on port: ${port}`);
+});
