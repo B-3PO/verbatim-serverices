09db0be0744ad540575b232999b2951f763b8bc9
Ben Rubin <ben@benjaminrubin.com>
2017-10-29 19:50:45 -0500

    use dummy data

--- server.js
+++ server.js
@@ -67,25 +67,65 @@ app.post('/add-item', (req, res) => {
 
 
 app.post('/get-cart', (req, res) => {
-  let elements = (req.tokenData.cart || []).map(i  => {
-    return {
-      title: 'Item ' + i.id,
-      subtitle: 'quntity: ' + i.quantity,
-      image_url: '',
-      buttons: [],
-      default_action: {}
-    };
-  });
+  // let elements = (req.tokenData.cart || []).map(i  => {
+  //   return {
+  //     title: 'Item ' + i.id,
+  //     subtitle: 'quntity: ' + i.quantity,
+  //     image_url: '',
+  //     buttons: [],
+  //     default_action: {}
+  //   };
+  // });
+  //
+  // res.send({
+  //  messages: [
+  //     {
+  //       attachment: {
+  //         type: 'template',
+  //         payload: {
+  //           template_type: 'list',
+  //           top_element_style: 'compact',
+  //           elements: elements
+  //         }
+  //       }
+  //     }
+  //   ]
+  // });
 
   res.send({
-   messages: [
+   "messages": [
       {
-        attachment: {
-          type: 'template',
-          payload: {
-            template_type: 'list',
-            top_element_style: 'compact',
-            elements: elements
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"list",
+            "top_element_style":"large",
+            "elements":[
+              {
+                "title":"Chatfuel Rockets T-Shirt",
+                "image_url":"http://rockets.chatfuel.com/img/shirt.png",
+                "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://rockets.chatfuel.com/store/shirt",
+                    "title":"View Item"
+                  }
+                ]
+              },
+              {
+                "title":"Chatfuel Rockets Hoodie",
+                "image_url":"http://rockets.chatfuel.com/img/hoodie.png",
+                "subtitle":"Soft gray cotton t-shirt with CF Rockets logo",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://rockets.chatfuel.com/store/hoodie",
+                    "title":"View Item"
+                  }
+                ]
+              }
+            ]
           }
         }
       }
