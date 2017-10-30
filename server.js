29c2de6974ae75cc2e758172a090e411ea62a902
Ben Rubin <ben@benjaminrubin.com>
2017-10-29 19:00:53 -0500

    add logs

--- server.js
+++ server.js
@@ -70,7 +70,8 @@ app.post('/add-item', (req, res) => {
 
 
 app.post('/get-cart', (req, res) => {
-  let elements = req.tokenData.map(i  => {
+  logger.info('/get-cart tokenData: ' + JSON.stringify(req.tokenData, null, 2));
+  let elements = (req.tokenData.cart || []).map(i  => {
     return {
       title: 'Item ' + i.id,
       subtitle: 'quntity: ' + i.quantity,
