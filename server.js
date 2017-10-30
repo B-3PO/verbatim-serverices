30f72e7794cb67fa08c71579ed683640498c2a50
Ben Rubin <ben@benjaminrubin.com>
2017-10-29 19:56:47 -0500

    make text res

--- server.js
+++ server.js
@@ -76,59 +76,20 @@ app.post('/get-cart', (req, res) => {
   //     default_action: {}
   //   };
   // });
-  //
-  // res.send({
-  //  messages: [
-  //     {
-  //       attachment: {
-  //         type: 'template',
-  //         payload: {
-  //           template_type: 'list',
-  //           top_element_style: 'compact',
-  //           elements: elements
-  //         }
-  //       }
-  //     }
-  //   ]
-  // });
 
   res.send({
-   "messages": [
-      {
-        "attachment":{
-          "type":"template",
-          "payload":{
-            "template_type":"list",
-            "top_element_style":"large",
-            "elements":[
-              {
-                "title":"Chatfuel Rockets T-Shirt",
-                "image_url":"http://rockets.chatfuel.com/img/shirt.png",
-                "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
-                "buttons":[
-                  {
-                    "type":"web_url",
-                    "url":"https://rockets.chatfuel.com/store/shirt",
-                    "title":"View Item"
-                  }
-                ]
-              },
-              {
-                "title":"Chatfuel Rockets Hoodie",
-                "image_url":"http://rockets.chatfuel.com/img/hoodie.png",
-                "subtitle":"Soft gray cotton t-shirt with CF Rockets logo",
-                "buttons":[
-                  {
-                    "type":"web_url",
-                    "url":"https://rockets.chatfuel.com/store/hoodie",
-                    "title":"View Item"
-                  }
-                ]
-              }
-            ]
-          }
+   messages: (req.tokenData.cart || []).map(i  => {
+     return {
+       text: 'Item ' + i.id,
+       subtitle: 'Qunatity: ' + i.quantity,
+       buttons: [
+        {
+          "type":"web_url",
+          "url":"https://rockets.chatfuel.com/store/shirt",
+          "title":"Remove Item"
         }
-      }
-    ]
+      ]
+     };
+   })
   });
 });
